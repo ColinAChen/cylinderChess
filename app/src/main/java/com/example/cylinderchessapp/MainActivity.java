5cbfc392c9c917941d74550c69e68a09c9cc7b3b
azhang36 <azhang36@ucsc.edu>
2019-02-11 22:12:19 -0800

    variables test

--- app/src/main/java/com/example/cylinderchessapp/MainActivity.java
+++ app/src/main/java/com/example/cylinderchessapp/MainActivity.java
@@ -5,13 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.cylinderchessapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("apples");
     }
 
     public void sendMessage (View view)
