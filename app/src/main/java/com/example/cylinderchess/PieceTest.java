82bd6f6694d4dfdf0e91c7f5be287e0b05ab0227
Colin Chen <colin.a.chen@gmail.com>
2019-02-16 23:53:12 -0800

    Updated piecetest

--- app/src/main/java/com/example/cylinderchess/PieceTest.java
+++ app/src/main/java/com/example/cylinderchess/PieceTest.java
@@ -3,10 +3,10 @@ import java.util.ArrayList; // import the ArrayList class
 public class PieceTest{
 	public static void main (String[] args){
 		//Pawn testPawn = new Pawn("p", false, 1,3,false);
-		Rook testKing = new Rook("k", false, 4, 5, false);
+		Pawn testKing = new Pawn("k", true, 6, 0, false);
 		//System.out.println(testPawn.isLegitMove(4,3));
 		ArrayList<int[]>possibleMoves = testKing.getPossibleMoves();
-		System.out.println(testKing.name);
+		System.out.printf("Possible moves for %s %s at row %d col % d%n",testKing.getColor(),testKing.name,testKing.x,testKing.y);
 		for (int[] pair : possibleMoves){
 			for (int element: pair){
 				System.out.print(element + " ");
