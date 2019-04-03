106e1b8d33b7d249c25c9da657be006919ffc460
azhang36 <azhang36@ucsc.edu>
2019-04-02 17:17:46 -0700

    toggle for cpu game or pvp game

--- app/src/main/java/com/example/chessApp/MainActivity.java
+++ app/src/main/java/com/example/chessApp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 
 import com.example.chessApp.R;
 import com.example.chessApp.cylinder.DisplayBoardCylinder;
@@ -12,14 +13,22 @@ import com.example.chessApp.normal.InstructionsNormal;
 
 public class MainActivity extends AppCompatActivity
 {
+    boolean cpuGame = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public void computerOption(View view)
+    {
+        boolean checked = ((CheckBox) view).isChecked();
+        cpuGame = checked;
+    }
+
     public void goToBoardNormal(View view) {
         Intent intent = new Intent(this, DisplayBoardNormal.class);
+        intent.putExtra("cpuToggle", cpuGame);
         startActivity(intent);
     }
 
